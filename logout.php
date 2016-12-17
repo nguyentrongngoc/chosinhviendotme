d5042fac780e0792d6f1c045779245f3e8de8852
Nguyen Thanh Toan <nguyenthanhtoan1412@gmail.com>
2016-12-17 16:36:53 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-session_start();  
-if (isset($_SESSION['id_member'])){
-    unset($_SESSION['id_member']); // xóa session login
-}
-header('Refresh:0; url=http://localhost:8080/chosinhvien/index.php');
-?>
